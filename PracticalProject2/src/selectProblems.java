eb34533fe0633fd964c8bc61c098d4be1bcb2029
topaz <topaz@172.16.139.1>
2019-05-27 13:42:54 +0300

    selectProblems

---
+++ PracticalProject2/src/selectProblems.java
@@ -0,0 +1,29 @@
+
+import javafx.util.Pair;
+import java.util.ArrayList;
+
+public class selectProblems {
+
+	/**
+	 * 
+	 */
+	public static Pair <Integer,Integer> selectRandQuickSort(int[] arr,int k)
+	{
+		
+	}
+	/**
+	 * 
+	 */
+	
+	/**
+	 * 
+	 */
+	
+	/**
+	 * 
+	 */
+	
+	/**
+	 * 
+	 */
+}
