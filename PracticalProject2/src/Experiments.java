4e13d0e2a15f979fdc157e61c2135f7e8916d4b2
computer <computer@computer-pc>
2019-05-30 22:40:57 +0300

    tring commit

--- PracticalProject2/src/Experiments.java
+++ PracticalProject2/src/Experiments.java
@@ -7,14 +7,31 @@ public class Experiments {
 	private static final int MAX_VALUE = 10000;
 	
 	public static enum method {
-		selectRandQuickSort,//("Random QuickSort"),
-		selectInsertionSort,//("Insertion Sort"),
-		selectHeap,//("Heap (single)"),
-		selectDoubleHeap,//("DoubleHeap"),
-		randQuickSelect,//("Random Quick Select"),
-		medOfMedQuickSelect//("Med Of Med Quick Select");
-		
-		//privtae final String displayName;
+		selectRandQuickSort,
+		selectInsertionSort,
+		selectHeap,
+		selectDoubleHeap,
+		randQuickSelect,
+		medOfMedQuickSelect
+	}
+	
+
+	private String methodToString(method mtd) {
+		switch(mtd) {
+		case selectRandQuickSort:
+			return "Random Quick Sort";
+		case selectInsertionSort:
+			return "Insertion Sort";
+		case selectHeap:
+			return "Heap";
+		case selectDoubleHeap:
+			return "Double Heap";
+		case randQuickSelect:
+			return "Random Quick Select";
+		case medOfMedQuickSelect:
+			return "Median Of Medians Quick Select";
+		}
+		return "";
 	}
 
 	public static int[] randomArray(int n) {
@@ -45,7 +62,21 @@ public class Experiments {
 
 	}
 	
-	private static int doExperiment() {
+	private static int doExperiment(method mtd) {
+		int[] array;
+		int k;
+		int n;
+		int comps;
+		for(int i = 1; i < 11; i++) {
+			n = i * MAX_VALUE;
+			array = randomArray(n);
+			k = 1;
+			for(int j = 0; j < 11; j++) {
+				
+				
+				k += n/10;
+			}
+		}
 		return 42;
 	}
 	
